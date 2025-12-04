94c8081afc3ad535b29b001a2a83626e9f2e0cdc
Sammyalhashe <sammy@salh.xyz>
2025-12-04 22:47:46 +0000

    fixed email.

--- components/interpolator.jsx
+++ components/interpolator.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import Proptypes from "prop-types";
 
 function Interpolator({ interpolatedResults }) {
-  const mappedResults = interpolatedResults.map((res, index) => {
-    const ind = res.cmd + res.result + index;
-    const cmd = res.cmd;
-    return (
-      <div key={ind} style={{width : (cmd === "budg") ? "100%" : "auto"}}>
-        <span className="prompt prompt-lg">
-          sammyalhashemi1@outlook.com$&nbsp;
-        </span>
-        <span className="prompt prompt-sm">$ </span>
-        <span>{res.cmd}</span>
-        <div>{res.result}</div>
-      </div>
-    );
-  });
-  return <div id="interpolator">{mappedResults}</div>;
+    const mappedResults = interpolatedResults.map((res, index) => {
+        const ind = res.cmd + res.result + index;
+        const cmd = res.cmd;
+        return (
+            <div key={ind} style={{ width: (cmd === "budg") ? "100%" : "auto" }}>
+                <span className="prompt prompt-lg">
+                    sammy@salh.xyz$&nbsp;
+                </span>
+                <span className="prompt prompt-sm">$ </span>
+                <span>{res.cmd}</span>
+                <div>{res.result}</div>
+            </div>
+        );
+    });
+    return <div id="interpolator">{mappedResults}</div>;
 }
 
 Interpolator.propTypes = {
-  interpolatedResults: Proptypes.arrayOf(
-    Proptypes.shape({
-      cmd: Proptypes.string,
-      result: Proptypes.node
-    })
-  ).isRequired
+    interpolatedResults: Proptypes.arrayOf(
+        Proptypes.shape({
+            cmd: Proptypes.string,
+            result: Proptypes.node
+        })
+    ).isRequired
 };
 
 export default Interpolator;
